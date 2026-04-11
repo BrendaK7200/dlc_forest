32d4caf401c466205179d9d7aa4aeebbe114ce08
BrendaK7200 <87945117+BrendaK7200@users.noreply.github.com>
2026-04-10 22:35:01 -0400

    Achievements

---
+++ achievements.lua
@@ -0,0 +1,48 @@
+local achievements = {
+    {
+        id = "forest_act1_complete",
+        name = "Evershade Forest",
+        desc = "Finish Act 1 of Evershade Forest, unlocking Brenda as a party member.",
+        hint = "Find out what's at the end of the forest.",
+        border = "common",
+        icon = "forest_act1_complete"
+    },
+
+    {
+        id = "forest_act2_complete",
+        name = "Nevermore",
+        desc = "Finish Act 2 of Evershade Forest, unlocking Celestial as a party member.",
+        hint = "Investigate where things first ended off.",
+        border = "uncommon",
+        icon = "placeholder"
+    },
+
+    {
+        id = "forest_act3_complete",
+        name = "What a Horrible Night for a Dark Fountain",
+        desc = "Finish Act 3 of Evershade Forest, awakening Brenda's full power.",
+        hint = "Go camping out in the woods.",
+        border = "rare",
+        icon = "placeholder"
+    },
+
+    {
+        id = "forest_perfect_sneaking",
+        name = "Metal Bear Solid",
+        desc = "Complete Act 1 without ever getting spotted by Bearserker.",
+        hint = "Become a master of stealth.",
+        border = "epic",
+        icon = "forest_perfect_sneaking"
+    },
+
+    {
+        id = "forest_bearserker_challenge",
+        name = "Valhalla Awaits",
+        desc = "Kill Bearserker while everyone in your party is LV 1.",
+        hint = "Fell a powerful foe as your first ever kill.",
+        border = "rare",
+        icon = "forest_bearserker_challenge"
+    }
+}
+
+return achievements
